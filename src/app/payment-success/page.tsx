15688c1a163f4fc4946d6ced22db36b69730bad6
Lukas Gonzales <lukasgonzales@Lukass-MacBook-Pro.local>
2024-08-20 11:29:31 -0600

    Fourth commit

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -8,8 +8,6 @@ const PaymentSuccess = ({
 }) => {
   const stripe = useStripe();
 
-  const charges = stripe?.retrieveOrder;
-
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
