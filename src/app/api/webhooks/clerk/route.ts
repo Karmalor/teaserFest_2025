752bbf3578dad84c89323a0c4c405a45d644a3e4
Lukas Gonzales <lukasgonzales@Lukass-MacBook-Pro.local>
2024-08-22 10:42:05 -0600

    123.

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -55,19 +55,21 @@ export async function POST(req: Request) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   console.log('Webhook body:', body)
 
-  if (eventType === 'user.created'){
+  if (eventType === "user.created"){
     const {id, email_addresses, image_url, first_name, last_name, username, } = evt.data
 
     console.log(evt.data)
 
-    // const user = {
-    //     clerkId: id,
-    //     email: email_addresses[0].email_address,
-    //     username: username!,
-    //     firstName: first_name!,
-    //     lastName: last_name!,
-    //     photo: image_url
-    // }
+    const user = {
+        clerkId: id,
+        email: email_addresses[0].email_address,
+        username: username!,
+        firstName: first_name!,
+        lastName: last_name!,
+        photo: image_url
+    }
+
+    console.log("User", user)
 
     // const newUser = await createUser(user)
 
