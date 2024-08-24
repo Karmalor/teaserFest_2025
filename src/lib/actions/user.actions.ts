9bb9fa53b133b0e6f25eb58fd2f12f6199e130f1
Lukas Gonzales <lukasgonzales@Lukass-MacBook-Pro.local>
2024-08-24 10:30:29 -0500

    Adding updateUser & deleteUser

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -3,7 +3,8 @@
 import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
 import { db } from "@/db";
-import { usersTable } from "@/db/schema";
+import { SelectUser, usersTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const createUser = async (user: CreateUserParams) => {
     try {
@@ -15,3 +16,26 @@ export const createUser = async (user: CreateUserParams) => {
         handleError(error)
     }
 }
+
+ export const updateUser = async (id: SelectUser['clerkId'], user: UpdateUserParams) => {
+    try {
+        const updatedUser =     await db.update(usersTable).set(user).where(eq(usersTable.clerkId, id));
+
+        return JSON.parse(JSON.stringify(updateUser))
+    } catch (error) {
+        handleError(error)
+    }
+
+    
+ }
+
+ export const deleteUser = async (id: SelectUser['clerkId']) => {
+    await db.delete(usersTable).where(eq(usersTable.clerkId, id));
+
+ }
+
+//  export async function updateUser(id: SelectUser['clerkId'], user: Partial<Omit<SelectUser, 'clerkId'>>) {
+//     await db.update(usersTable).set(user).where(eq(usersTable.clerkId, id));
+//   }
+
+
