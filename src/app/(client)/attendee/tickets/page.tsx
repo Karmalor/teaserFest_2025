b4a756e99f891f2ebbec4d480036c37e90bd7987
Lukas Gonzales <lukasgonzales@Lukass-MacBook-Pro.local>
2024-11-10 15:50:14 -0700

    Formating attendee tickets page

--- src/app/(client)/attendee/tickets/page.tsx
+++ src/app/(client)/attendee/tickets/page.tsx
@@ -5,6 +5,7 @@ import { getTicketByUser } from "@/lib/actions/ticket.actions";
 import { useUser } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
 import QRCard from "./_components/QRCard";
+import Link from "next/link";
 
 const PurchasedTicketsPage = () => {
   const { user } = useUser();
@@ -35,10 +36,15 @@ const PurchasedTicketsPage = () => {
   console.log(purchasedTickets);
 
   return (
-    <div className="flex justify-start items-center gap-4 mx-16 ">
-      {purchasedTickets.map((item, i) => (
-        <QRCard key={i} ticketData={item} />
-      ))}
+    <div>
+      <h1 className="text-xl m-8">Click to view ticket</h1>
+      <div className="flex flex-col sm:flex-row justify-start items-center gap-8 mx-auto sm:mx-16 mt-8">
+        {purchasedTickets.map((item, i) => (
+          <Link href={`/attendee/tickets/${item.id}`}>
+            <QRCard key={i} ticketData={item} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
